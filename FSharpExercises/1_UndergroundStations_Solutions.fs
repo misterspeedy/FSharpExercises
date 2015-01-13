d2af2ac2f3bdad9efb1f70247a7f694ac18a7d08
Kit <kit.eason@gmail.com>
2015-01-13 11:14:57 +0000

    Add some files!

---
+++ FSharpExercises/1_UndergroundStations_Solutions.fs
@@ -0,0 +1,29 @@
+﻿#if INTERACTIVE
+#else
+namespace FSharpExercises
+#endif
+
+module UndergroundSolutions = 
+
+   // You can 'open' a module like a namspace:
+   open Underground
+   
+   // Which stations contain "East"?
+   let EastStations() =
+      // Create a function which returns an array of stations containing "East"
+      Stations
+      |> Array.filter (fun name -> name.Contains "East")
+
+   // Which stations contain "North", "South", "East" or "West"?
+   let CompassStations() =
+      Stations
+      |> Array.filter (fun name -> name.Contains "North" || name.Contains "South" || name.Contains "East" || name.Contains "West")
+      
+   // Which stations contain "North", "South", "East" or "West"?
+   let CompassStationsNice() =
+      let compassPoints = [|"North"; "South"; "East"; "West";|]
+
+      Stations
+      |> Array.filter (fun name -> compassPoints 
+                                   |> Array.exists (fun point -> name.Contains point))
+      
---
+++ FSharpExercises/1_UndergroundStations_Stub.fs
@@ -0,0 +1,20 @@
+﻿#if INTERACTIVE
+#else
+namespace FSharpExercises
+#endif
+
+module UndergroundExercises = 
+
+   // You can 'open' a module like a namspace:
+   open Underground
+
+   // Which stations contain "East"?
+   let EastStations() =
+      // Create a function which returns an array of stations containing "East"
+      ()
+
+   // Which stations contain "North", "South", "East" or "West"?
+   let CompassStations() =
+      // Create a function which returns an array of stations containing "North", "South", "East" or "West".
+      // (Don't worry about constraining to whole words.)
+      ()
