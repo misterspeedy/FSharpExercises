31c4ede7f3227a129aa2eaa4581801f5cc8474f6
Kit <kit.eason@gmail.com>
2015-01-20 16:33:59 +0000

    Add Person exercise (covers records, option types, pattern matching)

---
+++ PersonName/Person.fs
@@ -0,0 +1,39 @@
+﻿#if INTERACTIVE
+#else
+namespace PersonName
+#endif
+
+// This is the person 'Name' record type from the presentation...
+type Name = 
+   { 
+      First : string
+      Middle : string Option
+      Last : string 
+   }
+
+// ...and this function prints the name in a sensible format
+module NameFormatting =
+
+   let FormatName (name : Name) =
+      match name.First, name.Middle, name.Last with
+      | f, Some m, l -> sprintf "%s, %s %s" l f m
+      | f, None, l -> sprintf "%s, %s" l f
+
+// Exercises:
+
+// 1) Alter the record type so that all of the fields are optional.
+//
+// 2) Note the effect this has on the FormatName function.
+//
+// 3) Edit FormatName so that it covers all the possible cases. The required output is now:
+//
+//    Last, middle and first populated: output is "LLLL, FFFF MMMM"
+//
+//    Last and either middle or first populated: output is "LLLL, XXXX" where XXXX is 
+//     whichever of middle and first that is populated
+//
+//    First and middle are populated but surname is not: output is "(No surname), FFFF MMMM
+//
+//    Only one field populated: output is "XXXX" where "XXXX" is the content of the one populated field
+//    All fields missing: output is "(Unknown)"
+//
---
+++ PersonName/Person_Solutions.fs
@@ -0,0 +1,31 @@
+﻿#if INTERACTIVE
+#else
+namespace PersonNameSolution
+#endif
+
+type Name = 
+   { 
+      First : string Option
+      Middle : string Option
+      Last : string Option
+   }
+
+module NameFormatting =
+
+   let FormatName (name : Name) =
+      match name.First, name.Middle, name.Last with
+      // Last, middle and first populated: output is "LLLL, FFFF MMMM"
+      | Some f, Some m, Some l -> sprintf "%s, %s %s" l f m
+      // Last and either middle or first populated: output is "LLLL, XXXX" where XXXX is 
+      // whichever of middle and first that is populated
+      | Some x, None, Some l
+      | None, Some x, Some l -> sprintf "%s, %s" l x
+      // First and middle are populated but surname is not: output is "(No surname), FFFF MMMM
+      | Some f, Some m, None -> sprintf "(No surname), %s %s" f m
+      // Only one field populated: output is "XXXX" where "XXXX" is the content of the one populated field
+      | Some s, None, None
+      | None, Some s, None
+      | None, None, Some s -> sprintf "%s" s // Actually this could just return s
+      // All fields missing: output is "(Unknown)"
+      | None, None, None -> "(Unknown)"
+
